7a8ad338ac543477f8919b4f4480c661c179993e
mohamed khalid <mohamed.khalid.gamal@gmail.com>
2024-04-01 21:22:46 +0200

    responsive

--- JS/main.js
+++ JS/main.js
@@ -2,14 +2,24 @@ let bar = document.querySelector(".bar");
 let close = document.querySelector(".close");
 let navs = document.querySelectorAll("header ul li");
 let nav = document.querySelector("header ul");
+let but1 = document.querySelectorAll(
+  "#packages > div.package.eco > div.mon > button",
+);
+let but2 = document.querySelectorAll(
+  "#packages > div.package.pro > div.mon > button",
+);
+let but3 = document.querySelectorAll(
+  "#packages > div.package.diamond > div.mon > button",
+);
+let price = document.querySelectorAll("#packages > div.package > div.price");
+// let done = false;
+// navs.forEach((e) => {
+//   e.addEventListener("click", () => {
+//     navs.forEach((e) => e.classList.remove("active"));
+//     e.classList.add("active");
+//   });
+// });
 
-console.log(navs);
-navs.forEach((e) => {
-  e.addEventListener("click", () => {
-    navs.forEach((e) => e.classList.remove("active"));
-    e.classList.add("active");
-  });
-});
 if (bar || close) {
   bar.addEventListener("click", () => {
     close.style.display = "block";
@@ -18,5 +28,35 @@ if (bar || close) {
   close.addEventListener("click", () => {
     close.style.display = "none";
     nav.style.width = "0%";
+  });
+}
+prices(but1);
+prices(but2);
+prices(but3);
+function prices(a) {
+  a.forEach((e) => {
+    e.addEventListener("click", function () {
+      a.forEach((btn) => btn.classList.remove("active"));
+      e.classList.add("active");
+      if (e == but1[0]) {
+        price[0].textContent = price[0].getAttribute("data-mon") + " EGY";
+      } else if (e == but1[1]) {
+        price[0].textContent = price[0].getAttribute("data-3mon") + " EGY";
+      } else if (e == but1[2]) {
+        price[0].textContent = price[0].getAttribute("data-6mon") + " EGY";
+      } else if (e == but2[0]) {
+        price[2].textContent = price[2].getAttribute("data-mon") + " EGY";
+      } else if (e == but2[1]) {
+        price[2].textContent = price[2].getAttribute("data-3mon") + " EGY";
+      } else if (e == but2[2]) {
+        price[2].textContent = price[2].getAttribute("data-6mon") + " EGY";
+      } else if (e == but3[0]) {
+        price[1].textContent = price[1].getAttribute("data-mon") + " EGY";
+      } else if (e == but3[1]) {
+        price[1].textContent = price[1].getAttribute("data-3mon") + " EGY";
+      } else if (e == but3[2]) {
+        price[1].textContent = price[1].getAttribute("data-6mon") + " EGY";
+      }
+    });
   });
 }
